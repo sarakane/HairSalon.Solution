822b51d295351d330ded51f9dad8de413b2fd610
Sara Kane <kane.sarak@gmail.com>
2020-07-31 10:50:09 -0700

    Add functionality for Stylist Index and StylistsController

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,13 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    
+  }
+}
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,8 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string ClientName { get; set; }
+
+    public string ClientPhoneNumber { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
