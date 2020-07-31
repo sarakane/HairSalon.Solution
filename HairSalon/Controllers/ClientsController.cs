dc0c5e8bfccf6f135a4458e205b5b7699832da1d
Sara Kane <kane.sarak@gmail.com>
2020-07-31 15:39:04 -0700

    Add page to add a client to a stylist

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,5 +1,6 @@
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,25 @@ namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
-    
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Create(int id)
+    {
+      ViewBag.StylistId = id;
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Stylists");
+    }
   }
 }
